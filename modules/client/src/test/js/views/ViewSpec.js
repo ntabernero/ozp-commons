1270829cab80c3793c6c8c466fc6ea28c0993a30
Nish Patel <nishantpatel611@gmail.com>
2013-01-18 11:28:44 -0500

    OZP-10: Migrate displaying a dashboard.
    Add more tests to base View

--- modules/client/src/test/js/views/ViewSpec.js
+++ modules/client/src/test/js/views/ViewSpec.js
@@ -1,18 +1,55 @@
 define(['views/View'], function(View) {
-    describe('ViewSpec', function() {
+
+    describe('View', function() {
 
         beforeEach(function(done) {
             // Stub test initialization method;  And any custom pre-test elements here.
             done();
         });
-    
-        it('Test base View.  Verify that it has a show and hide method.', function () {
+
+        it('should instantiate.', function () {
             var view = new View();
             
             expect(view).to.be.an('object');
+        });
+    
+        it('should have show and hide methods.', function () {
+            var view = new View();
+
             expect(view.show).to.be.a('function');
             expect(view.hide).to.be.a('function');
         });
+
+        it('should fire "show" event when executing show method.', function () {
+            var view = new View(),
+                spy = sinon.spy();
+
+            view.on('show', spy);
+            view.show();
+
+            expect(spy.calledOnce).to.be.ok();
+        });
+
+        it('should fire "hide" event when executing hide method.', function () {
+            var view = new View(),
+                spy = sinon.spy();
+
+            view.on('hide', spy);
+            view.hide();
+
+            expect(spy.calledOnce).to.be.ok();
+        });
+
+        it('should return a promise for when executing show and hide methods.', function () {
+            var view = new View();
+
+            expect(view.show()).to.not.have.property('resolve');
+            expect(view.show()).to.have.property('then');
+
+            expect(view.hide()).to.not.have.property('resolve');
+            expect(view.hide()).to.have.property('then');
+        });
     
     });
+
 });
