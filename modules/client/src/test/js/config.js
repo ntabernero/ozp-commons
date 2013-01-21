f71b1ba99af5cac092796a407c2892fdaaea7854
Nathaniel Tabernero <nathaniel.tabernero@nextcentury.com>
2013-01-21 14:41:03 -0500

    ozp-40 fixing Object.keys use which doesnt work in ff 3.6 and ie7

--- modules/client/src/test/js/config.js
+++ modules/client/src/test/js/config.js
@@ -1,5 +1,12 @@
-var tests = Object.keys(window.__testacular__.files).filter(function (file) {
-    return /Spec\.js$/.test(file);
+var tests = [];
+for (var file in window.__testacular__.files) {
+    if (/Spec\.js$/.test(file)) {
+        tests.push(file);
+    }
+}
+
+requirejs.config({
+    enforceDefine: true
 });
 
 require({
@@ -7,15 +14,15 @@ require({
     // !! Testacular serves files from '/base'
     baseUrl: '/base/src/main/js',
     paths: {
-      require: '../target/js/require',
-      backbone: '/base/target/js/backbone'
-      //text: '../lib/text'
+        require: '../target/js/require',
+        backbone: '/base/target/js/backbone'
+        //text: '../lib/text'
     },
     shim: {
         backbone: {
             exports: 'Backbone'
         }
     }
-  }, tests, function() {
+}, tests, function() {
     window.__testacular__.start();
 });
