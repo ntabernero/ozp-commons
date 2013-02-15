227431d4df240d79de04988f099a849933c14209
Lawerence Mize <lawerence.mize@nextcentury.com>
2013-02-15 18:01:26 -0500

    OZP-120: Restructuring some models to match newer backend models and urls; updating tests.

--- modules/client/src/main/webapp/js/models/WidgetDefinitionModel.js
+++ modules/client/src/main/webapp/js/models/WidgetDefinitionModel.js
@@ -23,7 +23,7 @@ function(Model) {
     
     var WidgetDefinitionModel = Model.extend({
 
-        urlRoot: '/widget-defs',
+        urlRoot: '/ozp/rest/owf/widget-defs',
 
         defaults: {
             "guid": null,
@@ -45,9 +45,9 @@ function(Model) {
             "visibleForLaunch": true,
             
             "originalName": null,
-            "sendableIntents": null,
-            "receivableIntents": null,
-            "tags": ""   
+            "sendableIntents": [],
+            "receivableIntents": [],
+            "tags": [] 
         }
 
     });
--- modules/client/src/test/js/collections/WidgetDefinitionsCollectionSpec.js
+++ modules/client/src/test/js/collections/WidgetDefinitionsCollectionSpec.js
@@ -17,6 +17,21 @@
 define(['models/WidgetDefinitionModel', 'collections/WidgetDefinitionsCollection'], function(WidgetDefinitionModel, WidgetDefinitionsCollection) {
     describe('WidgetDefinitionsCollectionSpec', function() {
     
+        beforeEach(function(done) {
+            this.collection = new WidgetDefinitionsCollection();
+            this.widget1 = new WidgetDefinitionModel({id: '1', displayName: 'Apple'});
+            this.widget2 = new WidgetDefinitionModel({id: '2', displayName: 'Chestnut'});
+            
+            this.server = sinon.fakeServer.create();
+            done();
+        });
+        
+        afterEach(function(done) {
+            this.server.restore();
+            
+            done();
+        });
+        
         it('creates a collection', function () {
             var wc = new WidgetDefinitionsCollection();
             expect(wc).to.be.an('object');
@@ -39,6 +54,48 @@ define(['models/WidgetDefinitionModel', 'collections/WidgetDefinitionsCollection
             expect(wc.at(0).get('displayName')).to.eql('Apple');
             expect(wc.at(1).get('displayName')).to.eql('Banana');
             expect(wc.at(2).get('displayName')).to.eql('Chestnut');
+        });
+        
+        it('generates a base url for an empty collection.', function () {
+            expect(this.collection.url).to.eql('/ozp/rest/owf/widget-defs');
+        });
+        
+        it('generates a url for model in collection with an id.', function () {
+            this.collection.add(this.widget1)
+            expect(this.collection.at(0).get('displayName')).to.eql('Apple');
+            expect(this.collection.at(0).url()).to.eql('/ozp/rest/owf/widget-defs/1');
+        });
+        
+        it("creates a POST request to the correct url for a save", function() {
+            this.collection.create({displayName: 'test widget'});
+            expect(this.server.requests.length).to.eql(1);
+            expect(this.server.requests[0].method).to.eql("POST");
+            expect(this.server.requests[0].url).to.eql("/ozp/rest/owf/widget-defs");
+        });
+        
+        it("creates a GET request to the correct url for a fetch", function() {
+            this.collection.fetch();
+            expect(this.server.requests.length).to.eql(1);
+            expect(this.server.requests[0].method).to.eql("GET");
+            expect(this.server.requests[0].url).to.eql("/ozp/rest/owf/widget-defs");
+        });
+        
+        it("creates a PUT request to the correct url for bulk updates", function() {
+            this.collection.add([this.widget1, this.widget2]);
+            this.collection.sync('update', this.collection);
+            expect(this.server.requests.length).to.eql(1);
+            expect(this.server.requests[0].method).to.eql("PUT");
+            expect(this.server.requests[0].url).to.eql("/ozp/rest/owf/widget-defs");
+            expect(JSON.parse(this.server.requests[0].requestBody).length).to.eql(2);
+        });
+        
+        it("creates a PUT request to the correct URL for a model update.", function() {
+            this.collection.add(this.widget1);
+            this.collection.at(0).save();
+            expect(this.server.requests.length).to.eql(1);
+            expect(this.server.requests[0].method).to.eql("PUT");
+            expect(this.server.requests[0].url).to.eql("/ozp/rest/owf/widget-defs/1");
+            expect(JSON.parse(this.server.requests[0].requestBody).displayName).to.eql('Apple');
         });
     });
 });
